90fba9b43770144301fd7d50b26d47a741a310df
Denis5201 <builukden@yandex.ru>
2022-12-07 22:59:37 +0700

    add logger for ExceptionMiddleware

--- FoodDelivery/Services/Exceptions/ExceptionMiddleware.cs
+++ FoodDelivery/Services/Exceptions/ExceptionMiddleware.cs
@@ -6,10 +6,12 @@ namespace FoodDelivery.Services.Exceptions
     public class ExceptionMiddleware
     {
         private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionMiddleware> _logger;
 
-        public ExceptionMiddleware(RequestDelegate next) 
+        public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger) 
         { 
             _next = next;
+            _logger = logger;
         }
 
         public async Task Invoke(HttpContext context)
@@ -20,26 +22,31 @@ namespace FoodDelivery.Services.Exceptions
             }
             catch (IncorrectDataException e)
             {
+                _logger.LogError(e.Message);
                 context.Response.StatusCode = StatusCodes.Status400BadRequest;
                 await context.Response.WriteAsJsonAsync(new Response { Status = "400", Message = e.Message });
             }
             catch (ItemNotFoundException e)
             {
+                _logger.LogError(e.Message);
                 context.Response.StatusCode = StatusCodes.Status404NotFound;
                 await context.Response.WriteAsJsonAsync(new Response { Status = "404", Message = e.Message });
             }
             catch (NoPermissionException e)
             {
+                _logger.LogError(e.Message);
                 context.Response.StatusCode = StatusCodes.Status403Forbidden;
                 await context.Response.WriteAsJsonAsync(new Response { Status = "403", Message = e.Message });
             }
             catch (ElementAlreadyExistsException e)
             {
+                _logger.LogError(e.Message);
                 context.Response.StatusCode = StatusCodes.Status400BadRequest;
                 await context.Response.WriteAsJsonAsync(new Response { Status = "400", Message = e.Message });
             }
             catch (Exception e)
             {
+                _logger.LogError(e.ToString());
                 context.Response.StatusCode = StatusCodes.Status500InternalServerError;
                 await context.Response.WriteAsJsonAsync(new Response
                 {
